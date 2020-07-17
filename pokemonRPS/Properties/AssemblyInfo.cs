a9f7069fac78ba1dfe3d7e599b9047d30a66371b
Jackson Cooper <jacksoncooper12@gmail.com>
2020-07-17 16:47:09 -0400

    Initial commit

---
+++ pokemonRPS/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("pokemonRPS")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("pokemonRPS")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("86204661-b847-4fb1-a262-34b157a754d2")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ pokemonRPS/script/main.js
@@ -0,0 +1,96 @@
+﻿const choices = document.querySelectorAll(".nChoice");
+const score = document.getElementById('score');
+const result = document.getElementById('result');
+const restart = document.getElementById('restart');
+const modal = document.querySelector('.modalT');
+const scoreboard = {
+    player: 0,
+    computer: 0
+}
+
+//Play game Function
+function play(e) {
+    restart.style.display = 'inline-block';
+    const playerChoice = e.target.id;
+    const computerChoice = getComputerChoice();
+    const winner = getWinner(playerChoice, computerChoice);
+    console.log(playerChoice, computerChoice, winner);
+    showWinner(winner, computerChoice);
+}
+
+//get computer choice
+function getComputerChoice() {
+    const rand = Math.random();
+    if (rand < 0.34) {
+        return 'char';
+    }
+    else if (rand <= 0.67) {
+        return 'blast';
+    }
+    else {
+        return 'venus';
+    }
+}
+
+//get Game Winner
+function getWinner(p, c) {
+    if (p == c) {
+        return 'draw';
+    }
+    else if (p === "char") {
+        if (c === 'blast') {
+            return 'computer'
+        }
+        else if (c === 'venus') {
+            return 'player'
+        }
+    }
+    else if (p === 'blast') {
+        if (c === 'venus') {
+            return 'computer'
+        }
+        else if (c === 'char') {
+            return 'player'
+        }
+    }
+    else if (p === 'venus') {
+        if (c === 'char') {
+            return 'computer'
+        }
+        else if (c === 'blast') {
+            return 'player'
+        }
+    }
+}
+
+function showWinner(winner, computerChoice) {;
+    if (winner === 'player') {
+        scoreboard.player++;
+        result.innerHTML = `<h1 class="text-win">You Win!</h1><p>Computer Chose</p><img src="img/${computerChoice}.png" height="200" />`;
+    }
+    else if (winner === 'computer') {
+        scoreboard.computer++;
+        result.innerHTML = `<h1 class="text-lose">You Lost</h1><p>Computer Chose</p><img src="img/${computerChoice}.png" height="200" />`;
+    }
+
+    else if (winner === 'draw') {
+        result.innerHTML = `<h1>It's a Draw</h1><p>Computer Also Chose</p><img src="img/${computerChoice}.png" height="200" />`;
+    }
+    score.innerHTML = `<div class="col text-center pScore"><p class="scoreSize">Player: ${scoreboard.player}</p></div><div class="col text-center cScore"><p class="scoreSize">Computer: ${scoreboard.computer}</p></div>`;
+    modal.style.display = 'block';
+}
+
+function clearModal(e) {
+    if (e.target == modal) {
+        modal.style.display = 'none'
+    }
+}
+
+//Event listeners
+choices.forEach(nChoice => nChoice.addEventListener('click', play));
+window.addEventListener('click', clearModal)
+$("#restart").on('click', function () {
+    scoreboard.player = 0;
+    scoreboard.computer = 0;
+    score.innerHTML = `<div class="col text-center pScore"><p class="scoreSize">Player: 0</p></div><div class="col text-center cScore"><p class="scoreSize">Computer: 0</p></div>`;
+})
